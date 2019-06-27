9aadb3e8d3d405de359b9949fe12cdb7d57a3ddb
Evan Plaice <evanplaice@gmail.com>
2019-06-27 00:12:11 -0600

    Style and render the Cart items

--- static/scripts/components/cart.js
+++ static/scripts/components/cart.js
@@ -12,18 +12,26 @@ export class Cart extends Component {
     // render this if cart is empty
     if(items.length === 0) {
       this.element.innerHTML = `
-        <h2>Cart:</h2>
+        <h2>Cart</h2>
+        <hr/>
         <p class="no-items">You're cart is currently empty</p>
       `;
       return;
     }
 
     this.element.innerHTML = `
-      <h2>Cart:</h2>
+      <h2>Shopping Cart</h2>
+      <hr/>
       <ul class="cart_items">
         ${items.map(item => {
           return `
-            <li>${item}<button aria-label="Delete this item">×</button></li>
+            <li class="cart_item">
+              <button class="cart_item-delete" aria-label="Delete this item">×</button>
+              <img class="cart_item-image" src="/images/products/${item.image}"/>
+              <div class="cart_item-name">${item.name}</div>
+              <input type="text" class="cart_item-quantity" value="${item.quantity}"></input>
+              <div class="cart_item-price">${item.price}</div>
+            </li>
           `
         }).join('')}
       </ul>
