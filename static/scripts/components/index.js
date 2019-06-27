244492f3a67eb58edc6d242a71edd7e57a8eb224
Evan Plaice <evanplaice@gmail.com>
2019-06-27 00:09:53 -0600

    Delete the Total component

--- static/scripts/components/index.js
+++ static/scripts/components/index.js
@@ -1,3 +1,2 @@
 export { Cart } from './cart.js';
 export { Products } from './products.js';
-export { Total } from './total.js';
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,5 @@
 import { PersistentStore } from './store/index.js';
-import { Cart, Products, Total } from './components/index.js';
+import { Cart, Products } from './components/index.js';
 
 const store = new PersistentStore('cart', {
   state: { items: [] },
@@ -27,24 +27,6 @@ const store = new PersistentStore('cart', {
 const cartElement = document.querySelector('#cart');
 const cart = new Cart({ store, element: cartElement });
 cart.render();
-
-// const totalElement = document.querySelector('.total');
-// const total = new total({ store, element: totalElement });
-// total.render();
-
-const formElement = document.querySelector('#form');
-const inputElement = document.querySelector('#input');
-formElement.addEventListener('submit', e => {
-  e.preventDefault();
-
-  let value = inputElement.value.trim();
-
-  if (value.length) {
-    store.dispatch('ADD', value);
-    inputElement.value = '';
-    inputElement.focus();
-  }
-});
 
 const productsElement = document.querySelector('#products');
 const products = new Products({ store, element: productsElement })
